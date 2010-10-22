0bb9eb7250ceb84c6ac3a91cadd46f07b5ad089d
Peter Henderson <peter.henderson@ldschurch.org>
2010-10-22 17:23:47 -0600

    Remove dependencies on python-oauth2 and httplib2

--- src/login.py
+++ src/login.py
@@ -2,24 +2,24 @@
 # By Peter Henderson <peter.henderson@ldschurch.org>
 
 # Requires:
-#   - python-oauth2 <http://github.com/simplegeo/python-oauth2>
-#   - httplib2 <http://code.google.com/p/httplib2/>
 #   - web.py <http://webpy.org/>
 
+import random
+import time
+import urllib
 import urllib2
 import urlparse
 
-import oauth2 as oauth
 import web
 
-import enunciate.identity
+from enunciate import identity
 
 consumer_key = 'WCQY-7J1Q-GKVV-7DNM-SQ5M-9Q5H-JX3H-CMJK'
 consumer_secret = ''
 
 familysearch_base_url = 'http://www.dev.usys.org'
 properties_url = familysearch_base_url + '/identity/v2/properties' + '?dataFormat=application/json'
-properties = enunciate.identity.parse(urllib2.urlopen(properties_url)).properties
+properties = identity.parse(urllib2.urlopen(properties_url)).properties
 request_token_url = properties["request.token.url"]
 access_token_url = properties["access.token.url"]
 authorize_url = properties["authorize.url"]
@@ -31,20 +31,35 @@ urls = (
 app = web.application(urls, globals())
 
 
+def oauth_request(url, consumer_key, consumer_secret, token_secret="", params={}):
+    url = list(urlparse.urlparse(url))
+    query = urlparse.parse_qs(url[4])
+    query.update(params)
+    query.update({
+                  "oauth_consumer_key": consumer_key,
+                  "oauth_nonce": str(random.randint(0, 99999999)),
+                  "oauth_signature_method": "PLAINTEXT",
+                  "oauth_signature": "%s&%s" % (consumer_secret, token_secret),
+                  "oauth_timestamp": str(int(time.time())),
+                 })
+    url[4] = urllib.urlencode(query)
+    url = urlparse.urlunparse(url)
+    response = urllib2.urlopen(url)
+    return response.getcode(), response.read()
+
+
 class login():
-    
+
     def GET(self):
         callback_url = web.ctx.home + '/authorized'
-        consumer = oauth.Consumer(consumer_key, consumer_secret)
-        client = oauth.Client(consumer)
-        client.set_signature_method(oauth.SignatureMethod_PLAINTEXT())
         
         # Step 1: Get a request token. This is a temporary token that is used for 
         # having the user authorize an access token and to sign the request to obtain 
         # said access token.
-        resp, content = client.request(request_token_url + "?oauth_callback=%s" % callback_url)
-        if resp['status'] != '200':
-            return 'Invalid response %s.' % resp['status']
+        status, content = oauth_request(request_token_url, consumer_key, consumer_secret,
+                                        params={"oauth_callback": callback_url})
+        if status != 200:
+            return 'Invalid response %s.' % status
         request_token = dict(urlparse.parse_qsl(content))
         web.setcookie('request_token', request_token['oauth_token'])
         web.setcookie('request_token_secret', request_token['oauth_token_secret'])
@@ -63,12 +78,13 @@ class authorized():
         # access token somewhere safe, like a database, for future use.
         cookies = web.cookies(request_token='', request_token_secret='')
         params = web.input(oauth_verifier='')
-        token = oauth.Token(cookies.request_token, cookies.request_token_secret)
-        token.set_verifier(params.oauth_verifier)
-        consumer = oauth.Consumer(consumer_key, consumer_secret)
-        client = oauth.Client(consumer, token)
         
-        resp, content = client.request(access_token_url)
+        status, content = oauth_request(access_token_url, consumer_key, consumer_secret,
+                                        token_secret=cookies.request_token_secret,
+                                        params={
+                                                "oauth_token": cookies.request_token,
+                                                "oauth_verifier": params.oauth_verifier,
+                                               })
         access_token = dict(urlparse.parse_qsl(content))
         return 'Your session is: %s' % access_token['oauth_token']
 
