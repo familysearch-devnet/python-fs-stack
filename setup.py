f94c996749e5356da5bed0f0e1b02aa6d8bdb4c4
Peter Henderson <peter.henderson@ldschurch.org>
2010-11-12 16:07:58 -0700

    Reindent setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
 from distutils.core import setup
 
-setup(name='python-fs-stack',
-      version='0.1',
-      description='Python wrapper for all FamilySearch APIs',
-      url='https://devnet.familysearch.org/downloads/sample-code',
-      author='Peter Henderson',
-      author_email='peter.henderson@ldschurch.org',
-      license='FamilySearch API License Agreement <https://devnet.familysearch.org/downloads/sample-code/sample-clients/sample-client-license>',
-      package_dir={'': 'src'},
-      packages=['enunciate'],
-      scripts=['src/login.py', 'src/login_web.py'],
-      )
+setup(
+    name='python-fs-stack',
+    version='0.1',
+    description='Python wrapper for all FamilySearch APIs',
+    url='https://devnet.familysearch.org/downloads/sample-code',
+    author='Peter Henderson',
+    author_email='peter.henderson@ldschurch.org',
+    license='FamilySearch API License Agreement <https://devnet.familysearch.org/downloads/sample-code/sample-clients/sample-client-license>',
+    package_dir={'': 'src'},
+    packages=['enunciate'],
+    scripts=['src/login.py', 'src/login_web.py'],
+)
