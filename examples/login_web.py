4ba33b7f9f6e5c2cca57a72214766ebec542a59f
Peter Henderson <peter.henderson@ldschurch.org>
2011-01-11 15:02:46 -0700

    Clean up spacing in login_web.py

--- examples/login_web.py
+++ examples/login_web.py
@@ -4,13 +4,11 @@
 # Requires:
 #   - web.py <http://webpy.org/>
 
-import web
-
 import familysearch
+import web
 
 user_agent = "LoginWebSample"
 developer_key = 'WCQY-7J1Q-GKVV-7DNM-SQ5M-9Q5H-JX3H-CMJK'
-
 familysearch_base_url = 'http://www.dev.usys.org'
 
 urls = (
