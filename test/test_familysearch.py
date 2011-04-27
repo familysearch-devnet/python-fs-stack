d39ad3bd45270741a2bcebce49b97ad57e25036a
Peter Henderson <peter.henderson@ldschurch.org>
2011-04-27 13:17:06 -0600

    Add first few unit tests

---
+++ test/test_familysearch.py
@@ -0,0 +1,76 @@
+import familysearch
+import unittest
+import urllib2
+import wsgi_intercept
+import os.path
+from wsgi_intercept.urllib2_intercept import WSGI_HTTPHandler, WSGI_HTTPSHandler
+
+data_dir = os.path.dirname(__file__)
+sample_person1 = open(os.path.join(data_dir, 'person1.json')).read()
+sample_person2 = open(os.path.join(data_dir, 'person2.json')).read()
+
+class TestFamilySearch(unittest.TestCase):
+
+    def setUp(self):
+        self.longMessage = True
+        self.agent = 'TEST_USER_AGENT'
+        self.key = 'FAKE_DEV_KEY'
+
+    def tearDown(self):
+        self.clear_request_intercpets()
+
+    def add_request_intercept(self, response, out_environ=None, status='200 OK',
+                              host='api.familysearch.org', port=443,
+                              headers={'Content-Type': 'application/json'}):
+        '''Globally install a request intercept returning the provided response.'''
+        if out_environ is None:
+            out_environ = {}
+        def mock_app(environ, start_response):
+            out_environ.update(environ)
+            start_response(status, dict(headers).items())
+            return iter(response)
+        wsgi_intercept.add_wsgi_intercept(host, port, lambda: mock_app)
+        return out_environ
+
+    def clear_request_intercpets(self):
+        '''Remove all installed request intercepts.'''
+        wsgi_intercept.remove_wsgi_intercept()
+
+    def install_intercept_handler(self, fs):
+        '''Add the request intercept handler to the given FamilySearch instance.'''
+        new_handlers = [WSGI_HTTPHandler(), WSGI_HTTPSHandler()] + fs.opener.handlers
+        fs.opener = urllib2.build_opener(*new_handlers)
+
+    def test_requires_user_agent(self):
+        self.assertRaises(TypeError, familysearch.FamilySearch, key=self.key)
+
+    def test_requires_dev_key(self):
+        self.assertRaises(TypeError, familysearch.FamilySearch, agent=self.agent)
+
+    def test_accepts_user_agent_and_dev_key(self):
+        familysearch.FamilySearch(agent=self.agent, key=self.key)
+
+    def test_changes_base(self):
+        self.add_request_intercept(sample_person1, host='www.dev.usys.org', port=80)
+        self.add_request_intercept(sample_person2, host='api.familysearch.org', port=443)
+        fs_dev = familysearch.FamilySearch(self.agent, self.key)
+        fs_prod = familysearch.FamilySearch(self.agent, self.key, base='https://api.familysearch.org')
+        self.install_intercept_handler(fs_dev)
+        self.install_intercept_handler(fs_prod)
+        person1 = fs_dev.person()
+        person2 = fs_prod.person()
+        self.assertNotEqual(person1, person2, 'base argument failed to change base URL')
+        self.assertEqual(person1['id'], 'ABCD-123', 'wrong person returned from default base')
+        self.assertEqual(person2['id'], 'EFGH-456', 'wrong person returned from production base')
+
+    def test_include_user_agent(self):
+        request_environ = self.add_request_intercept(sample_person1)
+        fs = familysearch.FamilySearch(self.agent, self.key, base='https://api.familysearch.org')
+        self.install_intercept_handler(fs)
+        fs.person()
+        self.assertIn(self.agent, fs.agent, 'user agent not included in internal user agent')
+        self.assertIn('HTTP_USER_AGENT', request_environ, 'user agent header not included in request')
+        self.assertIn(self.agent, request_environ['HTTP_USER_AGENT'], 'user agent not included in user agent header')
+
+if __name__ == '__main__':
+    unittest.main()
